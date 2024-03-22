f13b1ffdf57b04a4c7c578cf962b7d2d27f0128e
ChepkemoiS <memoi24sharon@gmail.com>
2024-03-22 12:10:52 +0300

    done

---
+++ lib/scoreboard_generator.py
@@ -0,0 +1,18 @@
+participants = [
+    {
+        'name': 'Habanero Hillary',
+        'chickenwings': 5,
+        'hamburgers': 17,
+        'hotdogs': 11
+    },
+    {
+        'name': 'Big Bob',
+        'chickenwings': 20,
+        'hamburgers': 4,
+        'hotdogs': 11
+    }
+]
+
+scoreboard = create_scoreboard(participants)
+
+print(scoreboard)
